1a7e35334edf5fc88d55bb3aad5b6e1426f5e57b
nico24687 <theonlynico@hotmail.com>
2018-05-02 16:35:24 -0600

    state changing from true to false when click the checkbox

---
+++ src/Guest.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Guest = (props) =>
+  <li>
+    <span>{props.name}</span>
+    <label>
+      <input type="checkbox"
+       checked={props.isConfirmed}
+       onChange={props.handleConfirmation}  /> Confirmed
+    </label>
+    <button>edit</button>
+    <button>remove</button>
+  </li>
+
+
+
+
+
+
+export default Guest
